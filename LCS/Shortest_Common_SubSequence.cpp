cf233a5f53b9373fce2b1660c72a3c9cf93f8cbb
Mohim-Singla <mohimsingla@gmail.com>
2021-04-11 17:56:48 +0530

    Adding Shortest_Common_SuperSeq

---
+++ LCS/Shortest_Common_SubSequence.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string x="abcdgh";
+    string y="abedfhr";
+    cout<<"String a: "<<x<<endl<<"String b: "<<y<<endl;
+
+    int mat[x.size()+1][y.size()+1];
+    for (int i=0;i<=x.size();i++)
+    {
+        for (int j=0;j<=y.size();j++)
+        {
+            if (i==0 || j==0)
+            {
+                mat[i][j]=0;
+                continue;
+            }
+            else
+            {
+                if (x[i-1]==y[j-1])
+                {
+                    mat[i][j]=1+mat[i-1][j-1];
+                }
+                else
+                {
+                    mat[i][j]=max(mat[i-1][j], mat[i][j-1]);
+                }
+            }
+
+        }
+    }
+    cout<<"\n\nLength of shortest common super-sequence: "<<x.size()+y.size()-mat[x.size()][y.size()]<<endl;
+
+    string subseq;
+    int i=x.size(), j=y.size();
+    while(i!=0 && j!=0)
+    {
+        if (x[i-1]==y[j-1])
+        {
+            subseq.push_back(x[i-1]);
+            //cout<<x[i-1];
+            i--;
+            j--;
+        }
+        else
+        {
+            if (mat[i-1][j]>mat[i][j-1])
+            {
+                i--;
+                subseq.push_back(x[i]);
+            }
+            else
+            {
+                j--;
+                subseq.push_back(y[j]);
+            }
+        }
+    }
+    reverse(subseq.begin(), subseq.end());
+    cout<<"Longest common subseq: "<<subseq<<endl<<endl;
+
+
+}
